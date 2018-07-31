a46dc4354150cf12d702cfcca56c4f6e87d9086c
Domenico Garofoli <d.garofoli@cantierecreativo.net>
2018-07-31 09:47:46 +0200

    Remove evil tabs. Run serverver before evil.

--- elisp/configs/global-setup.el
+++ elisp/configs/global-setup.el
@@ -1,11 +1,12 @@
-(require 'evil)
-(evil-mode t)
-(require 'evil-tabs)
-(global-evil-tabs-mode t)
-
 ;; Run server if not runnning, for emacsclient using
+;; Run server before evil!
 (load "server")
 (unless (server-running-p) (server-start))
+
+(require 'evil)
+(evil-mode t)
+;;(require 'evil-tabs)
+;;(global-evil-tabs-mode t)
 
 ;; prevent backup files creation
 (setq make-backup-files nil)
@@ -19,3 +20,8 @@
 ;; Navigate between windows using Alt-left, Alt-up, Alt-right
 ;; Must be after move-text bindings
 (windmove-default-keybindings 'meta)
+
+;;Position indicator
+(line-number-mode t)
+(column-number-mode t)
+(size-indication-mode t)
