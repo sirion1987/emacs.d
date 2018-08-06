f11c951f93c498ca353c5f481e4840a5d59867ac
Domenico Garofoli <d.garofoli@cantierecreativo.net>
2018-08-06 09:20:50 +0200

    Fix rjsx mode

--- elisp/modes/rjsx-mode.el
+++ elisp/modes/rjsx-mode.el
@@ -2,7 +2,7 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . rjsx-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . rjsx-mode))
 (add-hook 'rjsx-mode-hook (lambda () (modify-syntax-entry ?_ "w")))
-(setq js2-basic-offset 2)
+(setq js2-indent-level 2)
 (setq js2-strict-missing-semi-warning nil)
 (with-eval-after-load 'rjsx-mode
   (define-key rjsx-mode-map "<" nil)
