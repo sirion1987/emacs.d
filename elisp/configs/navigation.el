d035f8f61bdaf33ffc5cfccfcb2dcdf1ce692e2b
Domenico Garofoli <d.garofoli@cantierecreativo.net>
2018-09-17 12:08:22 +0200

    Replace standard emacs find file with helm-find-file

--- elisp/configs/navigation.el
+++ elisp/configs/navigation.el
@@ -1,7 +1,7 @@
 (require 'helm-projectile)
 (helm-projectile-on)
 
-(global-set-key "\C-x\C-g" 'helm-projectile-find-file)
+(global-set-key "\C-x\C-f" 'helm-projectile-find-file)
 (global-set-key "\C-x\C-a" 'helm-projectile-ag)
 (global-set-key "\C-x\C-p" 'helm-projectile-switch-project)
 
