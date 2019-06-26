1a0ba7a5831c885988abb8f48ebeef16f3531689
Domenico Garofoli <d.garofoli@cantierecreativo.net>
2019-06-26 15:54:54 +0200

    Create log snippets

---
+++ snippets/enh-ruby-mode/pi.el
@@ -0,0 +1,6 @@
+# -*- mode: snippet -*-
+# name : puts ....inspect
+# key: pi
+# --
+
+puts "${1:}: #{$1.inspect}"$0
---
+++ snippets/rjsx-mode/clv.el
@@ -0,0 +1,6 @@
+# -*- mode: snippet -*-
+# name : console.log('v: ', v);
+# key: clv
+# --
+
+console.log('${1:}:', $1)$0
